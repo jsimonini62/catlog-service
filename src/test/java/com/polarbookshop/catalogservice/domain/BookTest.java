488dd286c40d4a995cde873bc7914d91a2510c50
jay simonini <jay.simonini@gmail.com>
2023-10-25 20:11:18 -0400

    first cloud project

--- src/test/java/com/polarbookshop/catalogservice/domain/BookTest.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookTest.java
@@ -25,11 +25,11 @@ class BookTest {
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).isEmpty();
     }
-//    @Test
-//    void whenIsbnDefinedButIncorrectThenValidationFails() {
-//        var book = new Book("a234567890", "Title", "Author", 9.90);
-//        Set<ConstraintViolation<Book>> violations = validator.validate(book);
-//        assertThat(violations).hasSize(1);
-//        assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid");
-//    }
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails() {
+        var book = new Book("a234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid.");
+    }
 }
